879607ba7607fe21d54381ab213bbc5f0831b863
raw <87tawan@.com>
2024-01-09 17:24:35 -0300

    new

---
+++ app/components/card-imgs.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function CardImgs() {
+  return (
+    <div className="mt-10 flex gap-4">
+      <div className="h-40 w-40 bg-white"></div>
+      <div className="h-40 w-40 bg-white"></div>
+      <div className="h-40 w-40 bg-white"></div>
+    </div>
+  );
+}
+
+export default CardImgs;
